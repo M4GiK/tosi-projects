9c3dcb096a912dc75d6ad36fc2dabaebeb1b1e22
m4gik <m4giksoftware@gmail.com>
2014-02-24 20:20:45 +0100

    Define interface for encryption operations

--- rsa-algorithm/src/Encryption.java
+++ rsa-algorithm/src/Encryption.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 /**
  * Project RSA Algorithm.
  * Copyright Michał Szczygieł.
@@ -5,11 +7,29 @@
  */
 
 /**
- * TODO COMMENTS MISSING!
+ * This interface describe the basic operation with encryption algorithm.
  * 
  * @author Michał Szczygieł <michal.szczygiel@wp.pl>
  * 
  */
 public interface Encryption {
+
+    /**
+     * This method makes operation to achieve decrypted message.
+     * 
+     * @param valueToDecrypt
+     *            The value of which will be performed the decryption process.
+     * @return Decrypted value.
+     */
+    public BigInteger decrypt(BigInteger valueToDecrypt);
+
+    /**
+     * This method makes operation to create encrypted message.
+     * 
+     * @param valueToEncrypt
+     *            The value of which will be performed the encryption process
+     * @return Encrypted value.
+     */
+    public BigInteger encrypt(BigInteger valueToEncrypt);
 
 }
