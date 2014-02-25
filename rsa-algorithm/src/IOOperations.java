00fcc70ddb5bf98158c2542a4dfc0abbbf25c1d8
m4gik <m4giksoftware@gmail.com>
2014-02-26 00:18:15 +0100

    Add class responsible for IO operations

---
+++ rsa-algorithm/src/IOOperations.java
@@ -0,0 +1,17 @@
+/**
+ * Project RSA Algorithm.
+ * Copyright Michał Szczygieł.
+ * Created at Feb 25, 2014.
+ */
+
+/**
+ * 
+ * This class is responsible for safe operations on files. This class contatin
+ * the basic input/output operations.
+ * 
+ * @author Michał Szczygieł <michal.szczygiel@wp.pl>
+ * 
+ */
+public class IOOperations {
+
+}
