5aaa2eb12254f9c38c6fdc316599f0ff4fd60d03
m4gik <m4giksoftware@gmail.com>
2014-02-27 22:04:49 +0100

    Create simple test class for RSA encryption algorithm

--- rsa-algorithm/src/Main.java
+++ rsa-algorithm/src/Main.java
@@ -1,11 +1,17 @@
+import java.io.IOException;
 /**
  * Project RSA Algorithm.
  * Copyright Michał Szczygieł.
  * Created at Feb 24, 2014.
  */
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 /**
- * TODO COMMENTS MISSING!
+ * This class is resposnible for testing and representation data for RSA
+ * encryption algorithm.
  * 
  * @author Michał Szczygieł <michal.szczygiel@wp.pl>
  * 
@@ -13,11 +19,31 @@
 public class Main {
 
     /**
+     * This logger is responsible for the registration of events.
+     */
+    static final Logger logger = LogManager.getLogger(Main.class.getName());
+
+    /**
+     * This method sets up configuration for loggers.
+     */
+    private static void loggerSetup() {
+        BasicConfigurator.configure();
+    }
+
+    /**
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        loggerSetup();
+        RSA rsa = new RSA(1024);
 
+        try {
+            logger.info(IOOperations.encryptStream(
+                    IOOperations.readFile("data.txt"), rsa, 10));
+        } catch (IOException e) {
+            logger.debug(e);
+            logger.error(e);
+        }
     }
 
 }
