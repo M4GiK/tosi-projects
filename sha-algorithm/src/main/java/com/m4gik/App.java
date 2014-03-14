183b09ac1eeeed3fd567d31b5018e38ef34ec843
m4gik <m4giksoftware@gmail.com>
2014-03-14 23:28:53 +0100

    Create project realize SHA1 algorithm

---
+++ sha-algorithm/src/main/java/com/m4gik/App.java
@@ -0,0 +1,13 @@
+package com.m4gik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sha-algorithm/src/test/java/com/m4gik/SHA1Test.java
@@ -0,0 +1,17 @@
+/**
+ * Project RSA Algorithm.
+ * Copyright Michał Szczygieł.
+ * Created at Mar 14, 2014.
+ */
+
+package com.m4gik;
+
+/**
+ * TODO COMMENTS MISSING!
+ * 
+ * @author Michał Szczygieł <michal.szczygiel@wp.pl>
+ * 
+ */
+public class SHA1Test {
+
+}
