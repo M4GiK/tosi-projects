c51e1c837576f5d1e374ae93366d24c8c2a61157
m4gik <m4giksoftware@gmail.com>
2014-03-16 19:43:34 +0100

    Create test for SHA1 class using TDD

---
+++ sha-algorithm/src/main/java/com/m4gik/SHA1.java
@@ -0,0 +1,21 @@
+/**
+ * Project RSA Algorithm.
+ * Copyright Michał Szczygieł.
+ * Created at Mar 16, 2014.
+ */
+package com.m4gik;
+
+/**
+ * TODO COMMENTS MISSING!
+ * 
+ * @author Michał Szczygieł <michal.szczygiel@wp.pl>
+ * 
+ */
+public class SHA1 {
+
+    public String hash(String stringToHash) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- sha-algorithm/src/test/java/com/m4gik/SHA1Test.java
+++ sha-algorithm/src/test/java/com/m4gik/SHA1Test.java
@@ -6,12 +6,36 @@
 
 package com.m4gik;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 /**
- * TODO COMMENTS MISSING!
+ * This class contains JUnit tests for class {@link SHA1}.
  * 
  * @author Michał Szczygieł <michal.szczygiel@wp.pl>
  * 
  */
 public class SHA1Test {
 
+    private static final String EXAMPLE_TEXT = "The quick brown fox jumps over the lazy dog";
+    private static final String HASHED_EXAMPLE_TEXT = "2fd4e1c6 7a2d28fc ed849ee1 bb76e739 1b93eb12";
+
+    /**
+     * This test method checks SHA1 algorithm for given string, which should be
+     * correct with another algorithm based on SHA1.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void checkCorrectnessOfSHA1() throws Exception {
+        // given
+        SHA1 sha1 = new SHA1();
+        // when
+        String result = sha1.hash(EXAMPLE_TEXT);
+        // then
+        assertThat(result, is(equalTo(HASHED_EXAMPLE_TEXT)));
+    }
 }
