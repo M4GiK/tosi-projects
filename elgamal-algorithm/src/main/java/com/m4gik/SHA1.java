cb223a9269c76f4c05ebbbe6a5e1d32241ea6f35
m4gik <m4giksoftware@gmail.com>
2014-03-16 23:20:05 +0100

    Initialize elgamal-algorithm project

---
+++ elgamal-algorithm/src/main/java/com/m4gik/SHA1.java
@@ -0,0 +1,236 @@
+/**
+ * Project RSA Algorithm.
+ * Copyright Michał Szczygieł.
+ * Created at Mar 16, 2014.
+ */
+package com.m4gik;
+
+import java.security.spec.InvalidParameterSpecException;
+
+/**
+ * This class represents implementation of SHA1 algorithm. This class contain
+ * operations to create hash, for given array of characters.
+ * 
+ * @author Michał Szczygieł <michal.szczygiel@wp.pl>
+ * 
+ */
+public class SHA1 {
+
+    /**
+     * This method check if given block of bytes is modulo 64.
+     * 
+     * @param paddedData
+     *            The data to checks.
+     * @throws InvalidParameterSpecException
+     */
+    public static void checkData(byte[] paddedData)
+            throws InvalidParameterSpecException {
+        if (paddedData.length % 64 != 0) {
+            throw new InvalidParameterSpecException();
+        }
+    }
+
+    /**
+     * This method makes hash for given string. This method convert a string to
+     * a sequence of 16-word blocks, stored as an array. Append padding bits and
+     * the length, as described in the SHA1 standard.
+     * 
+     * @param stringToHash
+     *            The given string to generate hash for it.
+     * @return The SHA1 hash of given string.
+     * @throws InvalidParameterSpecException
+     */
+    public static String hash(String message)
+            throws InvalidParameterSpecException {
+        byte[] paddedData = padTheMessage(message.getBytes());
+        int[] h = { 0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0 };
+        int[] k = { 0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6 };
+
+        checkData(paddedData);
+
+        int passesReq = paddedData.length / 64;
+        byte[] work = new byte[64];
+
+        for (int passCntr = 0; passCntr < passesReq; passCntr++) {
+            System.arraycopy(paddedData, 64 * passCntr, work, 0, 64);
+            processTheBlock(work, h, k);
+        }
+
+        return intArrayToHexStr(h);
+    }
+
+    /**
+     * This method converts integer array to hex string.
+     * 
+     * @param data
+     *            The data to transform.
+     * @return The hexadecimal value of given integer array.
+     */
+    private static String intArrayToHexStr(int[] data) {
+        String output = "";
+        String tempStr = "";
+        int tempInt = 0;
+
+        for (int cnt = 0; cnt < data.length; cnt++) {
+
+            tempInt = data[cnt];
+
+            tempStr = Integer.toHexString(tempInt);
+
+            if (tempStr.length() == 1) {
+                tempStr = "0000000" + tempStr;
+            } else if (tempStr.length() == 2) {
+                tempStr = "000000" + tempStr;
+            } else if (tempStr.length() == 3) {
+                tempStr = "00000" + tempStr;
+            } else if (tempStr.length() == 4) {
+                tempStr = "0000" + tempStr;
+            } else if (tempStr.length() == 5) {
+                tempStr = "000" + tempStr;
+            } else if (tempStr.length() == 6) {
+                tempStr = "00" + tempStr;
+            } else if (tempStr.length() == 7) {
+                tempStr = "0" + tempStr;
+            }
+
+            output = output + tempStr;
+        }
+
+        return output;
+    }
+
+    /**
+     * This method makes pre-processing. Appends the bit '1' to the message i.e.
+     * by adding 0x80 if characters are 8 bits. append 0 ≤ k < 512 bits '0',
+     * thus the resulting message length (in bits) is congruent to 448 (mod 512)
+     * append ml, in a 64-bit big-endian integer. So now the message length is a
+     * multiple of 512 bits.
+     * 
+     * @param data
+     *            The bytes of message to pad.
+     * @return Padded message.
+     */
+    private static byte[] padTheMessage(byte[] data) {
+        int origLength = data.length;
+        int tailLength = origLength % 64;
+        int padLength = 0;
+
+        if ((64 - tailLength >= 9)) {
+            padLength = 64 - tailLength;
+        } else {
+            padLength = 128 - tailLength;
+        }
+
+        byte[] thePad = new byte[padLength];
+        thePad[0] = (byte) 0x80;
+        long lengthInBits = origLength * 8;
+
+        for (int cnt = 0; cnt < 8; cnt++) {
+            thePad[thePad.length - 1 - cnt] = (byte) ((lengthInBits >> (8 * cnt)) & 0x00000000000000FF);
+        }
+
+        byte[] output = new byte[origLength + padLength];
+
+        System.arraycopy(data, 0, output, 0, origLength);
+        System.arraycopy(thePad, 0, output, origLength, thePad.length);
+
+        return output;
+    }
+
+    /**
+     * This method convert a string to a sequence of 16-word blocks, stored as
+     * an array.
+     * 
+     * @param work
+     *            The array of 64 bytes.
+     * @param h
+     *            The array with temporary values.
+     * @param k
+     *            The array with values.
+     */
+    private static void processTheBlock(byte[] work, int[] h, int[] k) {
+        int[] w = new int[80];
+        int a, b, c, d, e, f;
+        int temp;
+
+        for (int outer = 0; outer < 16; outer++) {
+
+            temp = 0;
+
+            for (int inner = 0; inner < 4; inner++) {
+                temp = (work[outer * 4 + inner] & 0x000000FF) << (24 - inner * 8);
+                w[outer] = w[outer] | temp;
+            }
+        }
+
+        for (int j = 16; j < 80; j++) {
+            w[j] = rotateLeft(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1);
+        }
+
+        a = h[0];
+        b = h[1];
+        c = h[2];
+        d = h[3];
+        e = h[4];
+
+        for (int j = 0; j < 20; j++) {
+            f = (b & c) | ((~b) & d);
+            temp = rotateLeft(a, 5) + f + e + k[0] + w[j];
+            e = d;
+            d = c;
+            c = rotateLeft(b, 30);
+            b = a;
+            a = temp;
+        }
+
+        for (int j = 20; j < 40; j++) {
+            f = b ^ c ^ d;
+            temp = rotateLeft(a, 5) + f + e + k[1] + w[j];
+            e = d;
+            d = c;
+            c = rotateLeft(b, 30);
+            b = a;
+            a = temp;
+        }
+
+        for (int j = 40; j < 60; j++) {
+            f = (b & c) | (b & d) | (c & d);
+            temp = rotateLeft(a, 5) + f + e + k[2] + w[j];
+            e = d;
+            d = c;
+            c = rotateLeft(b, 30);
+            b = a;
+            a = temp;
+        }
+
+        for (int j = 60; j < 80; j++) {
+            f = b ^ c ^ d;
+            temp = rotateLeft(a, 5) + f + e + k[3] + w[j];
+            e = d;
+            d = c;
+            c = rotateLeft(b, 30);
+            b = a;
+            a = temp;
+        }
+
+        h[0] += a;
+        h[1] += b;
+        h[2] += c;
+        h[3] += d;
+        h[4] += e;
+    }
+
+    /**
+     * The method moves bites to left side.
+     * 
+     * @param value
+     *            The value to rotated.
+     * @param bits
+     *            The bits size.
+     * @return The rotated value.
+     */
+    final static int rotateLeft(int value, int bits) {
+        int q = (value << bits) | (value >>> (32 - bits));
+        return q;
+    }
+}
---
+++ elgamal-algorithm/src/test/java/com/m4gik/SHA1Test.java
@@ -0,0 +1,55 @@
+/**
+ * Project RSA Algorithm.
+ * Copyright Michał Szczygieł.
+ * Created at Mar 14, 2014.
+ */
+
+package com.m4gik;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.security.spec.InvalidParameterSpecException;
+
+import org.junit.Test;
+
+/**
+ * This class contains JUnit tests for class {@link SHA1}.
+ * 
+ * @author Michał Szczygieł <michal.szczygiel@wp.pl>
+ * 
+ */
+public class SHA1Test {
+
+    private static final String EXAMPLE_TEXT = "The quick brown fox jumps over the lazy dog";
+    private static final String HASHED_EXAMPLE_TEXT = "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12";
+
+    /**
+     * This test method checks SHA1 algorithm for given string, which should be
+     * correct with another algorithm based on SHA1.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void checkCorrectnessOfSHA1() throws Exception {
+        // when
+        String result = SHA1.hash(EXAMPLE_TEXT);
+        // then
+        assertThat(result, is(equalTo(HASHED_EXAMPLE_TEXT)));
+    }
+
+    /**
+     * This test method checks SHA1 algorithm for given string, and this method
+     * shouldn't return exeption
+     * 
+     * @throws Exception
+     */
+    @Test(
+            expected = InvalidParameterSpecException.class)
+    public void dataShouldBeMultipleOf64() throws Exception {
+        // when
+        SHA1.checkData(EXAMPLE_TEXT.getBytes());
+        // then ... throw new Exception
+    }
+}
