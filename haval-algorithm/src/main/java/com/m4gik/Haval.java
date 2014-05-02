493e5384552b29a1627c79792c1ea9d46b2c1a31
m4gik <m4giksoftware@gmail.com>
2014-05-02 23:50:04 +0200

    Add class for haval algorithm.

---
+++ haval-algorithm/src/main/java/com/m4gik/Haval.java
@@ -0,0 +1,46 @@
+package com.m4gik;
+
+public class Haval extends BaseHash {
+
+    protected Haval(String name, int hashSize, int blockSize) {
+        super(name, hashSize, blockSize);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Object clone() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected byte[] getResult() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected byte[] padBuffer() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected void resetContext() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean selfTest() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    protected void transform(byte[] in, int offset) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
