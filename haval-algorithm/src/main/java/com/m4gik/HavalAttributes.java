cc8b9b4aa1eb6d8993bfc23b5a3998f26c52f33e
m4gik <m4giksoftware@gmail.com>
2014-05-02 19:50:29 +0200

    Create class with attributes for haval algorithm,

---
+++ haval-algorithm/src/main/java/com/m4gik/HavalAttributes.java
@@ -0,0 +1,60 @@
+package com.m4gik;
+
+/**
+ * 
+ * This class contains static fields, attributes for {@link Haval} class.
+ * 
+ * @author Michał Szczygieł <michal.szczygiel@wp.pl>
+ * 
+ */
+public class HavalAttributes {
+
+    /**
+     * The value for produce hashes in lengths of 128 bits.
+     */
+    public static final int HAVAL_128_BIT = 16;
+
+    /**
+     * The value for produce hashes in lengths of 160 bits.
+     */
+    public static final int HAVAL_160_BIT = 20;
+
+    /**
+     * The value for produce hashes in lengths of 192 bits.
+     */
+    public static final int HAVAL_192_BIT = 24;
+
+    /**
+     * The value for produce hashes in lengths of 224 bits.
+     */
+    public static final int HAVAL_224_BIT = 28;
+
+    /**
+     * The value for produce hashes in lengths of 256 bits.
+     */
+    public static final int HAVAL_256_BIT = 32;
+
+    /**
+     * The value specify the number of 3 rounds which are used to generate the
+     * hash.
+     */
+    public static final int HAVAL_3_ROUND = 3;
+
+    /**
+     * The value specify the number of 4 rounds which are used to generate the
+     * hash.
+     */
+    public static final int HAVAL_4_ROUND = 4;
+
+    /**
+     * The value specify the number of 5 rounds which are used to generate the
+     * hash.
+     */
+    public static final int HAVAL_5_ROUND = 5;
+
+    /**
+     * The value specify the version of algorithm.
+     */
+    public static final int HAVAL_VERSION = 1;
+
+}
