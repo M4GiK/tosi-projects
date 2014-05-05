9e9f22dd72f3ca736575d0216a5a29987c56919e
m4gik <m4giksoftware@gmail.com>
2014-05-05 13:40:12 +0200

    Add class for utils methods.

---
+++ haval-algorithm/src/main/java/com/m4gik/util/Util.java
@@ -0,0 +1,12 @@
+package com.m4gik.util;
+
+/**
+ * A collection of utility methods used throughout this project. This class
+ * mainly contains method for implementing the operations of presenting data.
+ * 
+ * @author Michał Szczygieł <michal.szczygiel@wp.pl>
+ * 
+ */
+public class Util {
+
+}
