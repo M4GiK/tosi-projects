c1a6bcc1d2a4353728d736df77812c2f605a7f3b
m4gik <m4giksoftware@gmail.com>
2014-05-05 13:58:11 +0200

    Add base static fields and private constructor.

--- haval-algorithm/src/main/java/com/m4gik/util/Util.java
+++ haval-algorithm/src/main/java/com/m4gik/util/Util.java
@@ -9,4 +9,26 @@ package com.m4gik.util;
  */
 public class Util {
 
+    /**
+     * Base-64 chars.
+     */
+    private static final String BASE64_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz./";
+
+    /**
+     * Base-64 charset.
+     */
+    private static final char[] BASE64_CHARSET = BASE64_CHARS.toCharArray();
+
+    /**
+     * Hex charset.
+     */
+    private static final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
+
+    /**
+     * Trivial constructor to enforce Singleton pattern.
+     */
+    private Util() {
+        super();
+    }
+
 }
