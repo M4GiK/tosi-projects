741f9c82a9ea5c23bfb52fcb9cd2e98a3a78e356
m4gik <m4giksoftware@gmail.com>
2014-05-12 23:31:44 +0200

    Extend test for Haval algorithm.

--- haval-algorithm/src/test/java/com/m4gik/HavalTest.java
+++ haval-algorithm/src/test/java/com/m4gik/HavalTest.java
@@ -28,15 +28,77 @@ public class HavalTest {
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-                { "The quick brown fox jumps over the lazy dog",
-                        "713502673d67e5fa557629a71d331945",
-                        HavalAttributes.HAVAL_128_BIT,
-                        HavalAttributes.HAVAL_3_ROUND },
-                { "The quick brown fox jumps over the lazy dog",
-                        "6eece560a2e8d6b919e81fe91b0e7156",
-                        HavalAttributes.HAVAL_128_BIT,
-                        HavalAttributes.HAVAL_4_ROUND } });
+        return Arrays
+                .asList(new Object[][] {
+                        { "The quick brown fox jumps over the lazy dog",
+                                "713502673d67e5fa557629a71d331945",
+                                HavalAttributes.HAVAL_128_BIT,
+                                HavalAttributes.HAVAL_3_ROUND },
+                        { "The quick brown fox jumps over the lazy dog",
+                                "6eece560a2e8d6b919e81fe91b0e7156",
+                                HavalAttributes.HAVAL_128_BIT,
+                                HavalAttributes.HAVAL_4_ROUND },
+                        { "The quick brown fox jumps over the lazy dog",
+                                "696f02111f2e1da5c21d50eb782b7e8f",
+                                HavalAttributes.HAVAL_128_BIT,
+                                HavalAttributes.HAVAL_5_ROUND },
+                        { "The quick brown fox jumps over the lazy dog",
+                                "b338ac397e8bccadcccd96549cadd4882d834107",
+                                HavalAttributes.HAVAL_160_BIT,
+                                HavalAttributes.HAVAL_3_ROUND },
+                        { "The quick brown fox jumps over the lazy dog",
+                                "6e739d01f5739ceed94da1a115b52d5951280560",
+                                HavalAttributes.HAVAL_160_BIT,
+                                HavalAttributes.HAVAL_4_ROUND },
+                        { "The quick brown fox jumps over the lazy dog",
+                                "ecce9fa8a428866304ff082af2f9062637d36b23",
+                                HavalAttributes.HAVAL_160_BIT,
+                                HavalAttributes.HAVAL_5_ROUND },
+                        {
+                                "The quick brown fox jumps over the lazy dog",
+                                "58e6ced002e311172483d434ba738ad033e7fa950e431503",
+                                HavalAttributes.HAVAL_192_BIT,
+                                HavalAttributes.HAVAL_3_ROUND },
+                        {
+                                "The quick brown fox jumps over the lazy dog",
+                                "228ee09bc7e36151c6f285f558e6aede66ad38c8341592b9",
+                                HavalAttributes.HAVAL_192_BIT,
+                                HavalAttributes.HAVAL_4_ROUND },
+                        {
+                                "The quick brown fox jumps over the lazy dog",
+                                "023d045f75d4bf051fd6e50f7b7417bf9949c4b5d2b4b7ef",
+                                HavalAttributes.HAVAL_192_BIT,
+                                HavalAttributes.HAVAL_5_ROUND },
+                        {
+                                "The quick brown fox jumps over the lazy dog",
+                                "e1d5792306f56b22419662b06d1885a66dca3eba01f53274c89aeaeb",
+                                HavalAttributes.HAVAL_224_BIT,
+                                HavalAttributes.HAVAL_3_ROUND },
+                        {
+                                "The quick brown fox jumps over the lazy dog",
+                                "dddd6689885f6db4ad91e35a35e1f4498446510df798d4fd54b8654f",
+                                HavalAttributes.HAVAL_224_BIT,
+                                HavalAttributes.HAVAL_4_ROUND },
+                        {
+                                "The quick brown fox jumps over the lazy dog",
+                                "03d953298c8e56b46385c6761cd4b2e377889a75c97eaea475421c73",
+                                HavalAttributes.HAVAL_224_BIT,
+                                HavalAttributes.HAVAL_5_ROUND },
+                        {
+                                "The quick brown fox jumps over the lazy dog",
+                                "9446028f42b3768a41bd873ca69b0c006341d986613567f39eb61f96ca683300",
+                                HavalAttributes.HAVAL_256_BIT,
+                                HavalAttributes.HAVAL_3_ROUND },
+                        {
+                                "The quick brown fox jumps over the lazy dog",
+                                "c0d4c6ea514105fd1a9c38a238553fb7fa21d4127eb1a3035a75ce9d06a83d96",
+                                HavalAttributes.HAVAL_256_BIT,
+                                HavalAttributes.HAVAL_4_ROUND },
+                        {
+                                "The quick brown fox jumps over the lazy dog",
+                                "b89c551cdfe2e06dbd4cea2be1bc7d557416c58ebb4d07cbc94e49f710c55be4",
+                                HavalAttributes.HAVAL_256_BIT,
+                                HavalAttributes.HAVAL_5_ROUND } });
     }
 
     private final String actualHash;
@@ -62,8 +124,7 @@ public class HavalTest {
     @Test
     public void testCheckStaticHashing() {
         // When
-        String hash = Haval.hash(input.getBytes(),
-                HavalAttributes.HAVAL_128_BIT, HavalAttributes.HAVAL_3_ROUND);
+        String hash = Haval.hash(input.getBytes(), size, rounds);
         // Then
         assertThat(hash, is(equalTo(expectedHash)));
     }
@@ -88,7 +149,7 @@ public class HavalTest {
 
     @Test
     public void testHashIsValid() {
-        assertThat(actualHash, is(equalTo(expectedHash)));
+        assertThat(actualHash.toLowerCase(), is(equalTo(expectedHash)));
     }
 
     @Test
